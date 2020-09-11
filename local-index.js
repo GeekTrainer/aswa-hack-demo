54fa22f3a3397ed590d7c7014198bbe6f03ddc87
Christopher Harrison <chrhar@microsoft.com>
2020-09-11 15:37:05 -0700

    Initial commit

---
+++ local-index.js
@@ -0,0 +1,26 @@
+const dogs = [
+    { name: 'Roscoe' },
+    { name: 'Sammy' },
+    { name: 'Pookie' },
+    { name: 'Artichokes' },
+]
+
+main();
+
+function main() {
+    displayDogs();
+}
+
+function displayDogs() {
+    for (let dog of dogs) {
+        addDogToUI(dog);
+    }
+}
+
+function addDogToUI(dog) {
+    const dogUI = document.createElement('a');
+    dogUI.className = 'list-group-item';
+    dogUI.innerText = dog.name;
+
+    document.getElementById('dogs-list').appendChild(dogUI);
+}
